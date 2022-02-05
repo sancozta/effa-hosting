810d32084409ba6eba6d4af44013e22323fe9e3c
Samuel Costa <sancozta@gmail.com>
2022-02-05 13:20:12 -0300

    Atualização

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from '../environments/environment';
+
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
+  ],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'effa-ddd4c',
+    appId: '1:640400464652:web:25d2ee88f3de3e5d558729',
+    storageBucket: 'effa-ddd4c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBlWHE3oxJ2eRrhKxMg2wyBuc1AcyGbJxg',
+    authDomain: 'effa-ddd4c.firebaseapp.com',
+    messagingSenderId: '640400464652',
+    measurementId: 'G-CKJLHK8JC4',
+  },
+};
