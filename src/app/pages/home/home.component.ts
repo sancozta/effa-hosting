3f5d04d990ebf435eda931aba0c459c07f7a214a
Samuel Costa <sancozta@gmail.com>
2022-02-11 17:03:49 -0300

    Atualização

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilsService } from './../../shared/services/utils.service';
 
 @Component({
   selector: 'mat-home',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private utils: UtilsService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.utils.generateTags({
+      title: 'Sanlabz - Projetos e Soluções',
+      description: 'Projetos e Soluções de Sistemas Web e Mobile',
+      image: ''
+    });
+  }
 }
--- src/app/shared/services/utils.service.ts
+++ src/app/shared/services/utils.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Title, Meta } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class UtilsService {
 
   constructor(
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private title: Title,
+    private meta: Meta,
   ) { }
 
   snackBar(message: string) {
@@ -30,5 +34,19 @@ export class UtilsService {
       return ''
     }
     return text.substring(0, 1);
+  }
+
+  generateTags({ title = '', description = '', image = '' }) {
+    this.title.setTitle(title);
+    this.meta.addTags([
+      { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'og:url', content: `https://${environment.firebase.authDomain}` },
+      { name: 'og:title', content: title },
+      { name: 'og:description', content: description },
+      { name: 'og:image', content: image },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@sanlabz' },
+    ]);
   }
 }
